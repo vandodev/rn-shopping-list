53aa308bf39a518cc144e0ccdfbdd68a156285d1
vandodev <vandoaparecido@hotmail.com>
2025-05-02 08:38:16 -0300

    :memo: :pencil: :book: Content

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -8,8 +8,11 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
-      <Input placeholder="O que você precisa comprar?" />
-      <Button title="Entrar" />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+      <View style={styles.content}></View>
     </View>
   );
 }
