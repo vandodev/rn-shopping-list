ad761d77ed94070d6b8612ae6cccdde8d67dcadc
vandodev <vandoaparecido@hotmail.com>
2025-05-02 07:40:49 -0300

    :memo: :pencil: :book: Utilizando Propriedades no Componente

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -6,7 +6,7 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
-      <Button />
+      <Button title="Entrar" />
     </View>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,14 @@
-import { Text, TouchableOpacity } from "react-native"
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native"
 import { styles } from "./styles"
 
-export function Button() {
+type Props = TouchableOpacityProps & {
+  title: string
+}
+
+export function Button({ title, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.8}>
-      <Text style={styles.title}>Adicionar</Text>
+    <TouchableOpacity style={styles.container} activeOpacity={0.8} {...rest}>
+      <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
   )
 }
