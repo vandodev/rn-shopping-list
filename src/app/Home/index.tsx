ca66c0ccb0b9d2fbf6e3b2861163b7f771ec28b2
vandodev <vandoaparecido@hotmail.com>
2025-05-02 08:15:27 -0300

    :memo: :pencil: :book: Componente Input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,11 +1,14 @@
 import { Image, View } from "react-native"
 import { styles } from "./styles"
+
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input"
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
+      <Input placeholder="O que você precisa comprar?" />
       <Button title="Entrar" />
     </View>
   );
