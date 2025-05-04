65854f65945fcbb061c69654d84523f5c2aad18a
vandodev <vandoaparecido@hotmail.com>
2025-05-04 08:20:54 -0300

    :memo: :pencil: :book: Lista de Itens

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -8,7 +8,23 @@ import { Filter } from "@/components/Filter"
 import { Item } from "@/components/Item"
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
-const ITEMS = Array.from({ length: 100 }).map((_, index) => String(index))
+const ITEMS = [
+  {
+    id: "1",
+    status: FilterStatus.DONE,
+    description: "1 pacote de café"
+  },
+  {
+    id: "2",
+    status: FilterStatus.PENDING,
+    description: "3 pacotes de macarrão"
+  },
+  {
+    id: "3",
+    status: FilterStatus.PENDING,
+    description: "3 cebolas"
+  },
+]
 
 export function Home() {
   return (
@@ -32,14 +48,19 @@ export function Home() {
 
         <FlatList 
           data={ITEMS}
-          keyExtractor={item => item}
+          keyExtractor={item => item.id}
           renderItem={({ item }) => (
             <Item
-              data={{ status: FilterStatus.DONE, description: String(item) }} 
+              data={item}  
               onStatus={() => console.log("mudar o status")}
               onRemove={() => console.log("remover")}
             />
           )}
+
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => <Text style={styles.empty}>Nenhum item aqui.</Text>}
         />
         
       </View>
